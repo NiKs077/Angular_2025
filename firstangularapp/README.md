84cc63fd5c5b4450e0624351ed82e425fcc527ec
niks <bankarniks7@gmail.com>
2025-02-18 12:34:34 +0530

    adding changes

--- firstangularapp/README.md
+++ firstangularapp/README.md
@@ -1,4 +1,4 @@
-# Todolist
+# Firstangularapp
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.0.7.
 
--- firstangularapp/src/app/app.component.spec.ts
+++ firstangularapp/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'todolist' title`, () => {
+  it(`should have as title 'firstangularapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todolist');
+    expect(app.title).toEqual('firstangularapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, todolist');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, firstangularapp');
   });
 });
